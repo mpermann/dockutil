d415c2905540e8c02de2330a9dcf3c6d18298884
Michael Permann <mike.permann@gmail.com>
2022-03-26 21:59:37 -0500

    Build 3.0.2

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-xcodebuild -project dockutil.xcodeproj -scheme dockutil -configuration Release archive DEVELOPMENT_TEAM="Z5J8CJBUWC"
+xcodebuild -project dockutil.xcodeproj -scheme dockutil -configuration Release archive DEVELOPMENT_TEAM="2C4P3AWW6F"
